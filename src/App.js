42cfa2518f599f8b35419ac67ec47a2ed9dfd7ff
Carlos Sepulveda <sepulveda.salazar.carlos@gmail.com>
2024-04-10 20:41:22 +1000

    upd

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import React from 'react';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
+import MainContent from './components/MainContent';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-   <Router>
+    <Router>
     <div>
       <Header/>
       <Routes>
@@ -15,6 +17,10 @@ function App() {
         <Route path="/about" element={<About/>}/>
       </Routes>
     </div>
+    <div>
+      <MainContent/>
+    </div>
+    <Footer/>
    </Router>
   );
 }
