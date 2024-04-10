b657c0ddfa148126bf8a93542d8fcedf604d78d5
Carlos Sepulveda <sepulveda.salazar.carlos@gmail.com>
2024-04-10 16:34:31 +1000

    personal

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+//About.js
+import React from 'react';
+
+const About = () => {
+    return(
+        <div>
+            <h2>Welcome To My Website</h2>
+            <p>Software engineer learning React.js</p>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+//Home.js
+import React from 'react';
+
+const Home = () => {
+    return(
+        <div>
+            <h2>Welcome To My Website</h2>
+            <p>Software engineer learning React.js</p>
+        </div>
+    );
+};
+
+export default Home;
